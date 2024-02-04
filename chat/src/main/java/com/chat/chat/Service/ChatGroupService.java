0fb8d6166849354f921bbcb05a03b5d73c32a381
Geethamadhuri16 <84759801+Geethamadhuri16@users.noreply.github.com>
2024-02-04 15:28:22 +0530

    Update ChatGroupService.java

--- chat/src/main/java/com/chat/chat/Service/ChatGroupService.java
+++ chat/src/main/java/com/chat/chat/Service/ChatGroupService.java
@@ -9,7 +9,7 @@ import com.chat.chat.Model.User;
 
 public interface ChatGroupService {
 	public ResponseEntity<Long> createGroup(ChatGroup c);
-	public ResponseEntity<String> dismantleGroup(Long id);
+	public ResponseEntity<String> dismantleGroup(Long id,Long uid);
 	public ResponseEntity<String> changeDesc(String desc,Long id);
 	public ResponseEntity<String> changeGroupName(String name,Long id);
 
